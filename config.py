cb18663c35563c11f63d6cf8a6a9504749cc419d
Владимир <134792878+AmungUnRaman@users.noreply.github.com>
2023-10-30 21:05:55 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '6354137307:AAFb8QSaXppGfrloK2dtaHoH0qbXk_YGZoI'
+
+keys = {'биткоин': 'BTC',
+        'эфириум': 'ETH',
+        'доллар': 'USD',
+        'евро': 'EUR',
+        'бинанс_коин': 'BNB',
+        'эфириум_классик': 'ETC',
+        'тефер': 'USDT'}
